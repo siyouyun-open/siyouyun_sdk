3aceb7f1b6b71400fa63faf49ce9242bfc5ea65c
liuweike <nervtech@163.com>
2022-11-04 17:04:10 +0800

    update api tmpl

--- api/api_define.go
+++ api/api_define.go
@@ -1,10 +1,16 @@
 package api
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+)
 
 type SiyouFaasApi map[string]func(iris.Context)
 
-var ApiExport = SiyouFaasApi{}
+func NewSiyouyunApi() SiyouFaasApi {
+	var api SiyouFaasApi
+	// maybe do something
+	return api
+}
 
 func (api SiyouFaasApi) AddRouter(uri string, f func(iris.Context)) {
 	api[uri] = f
