9a05420f22e64d7f54709ff47a5873bd91fe5723
wangqiang <shivaqiang@qq.com>
2022-11-05 14:38:18 +0800

    add mysql log

--- app.go
+++ app.go
@@ -4,6 +4,7 @@ import (
 	"github.com/kataras/iris/v12"
 	"github.com/siyouyun-open/siyouyun_sdk/entity"
 	"github.com/siyouyun-open/siyouyun_sdk/gateway"
+	siyoumysql "github.com/siyouyun-open/siyouyun_sdk/mysql"
 	"github.com/siyouyun-open/siyouyun_sdk/restclient"
 	"github.com/siyouyun-open/siyouyun_sdk/utils"
 	"gorm.io/driver/mysql"
@@ -39,7 +40,9 @@ func (a *App) init() {
 	if err != nil {
 		panic(err)
 	}
-	db, _ := gorm.Open(mysql.Open(a.AppInfo.DSN), &gorm.Config{})
+	db, _ := gorm.Open(mysql.Open(a.AppInfo.DSN), &gorm.Config{
+		Logger: siyoumysql.NewLogger(),
+	})
 	a.db = db
 }
 
--- mysql/main.go
+++ mysql/main.go
@@ -1,1 +1,20 @@
 package mysql
+
+import (
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
+)
+
+func NewLogger() logger.Interface {
+	return logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             time.Second,  // 慢 SQL 阈值
+			LogLevel:                  logger.Error, // 日志级别
+			IgnoreRecordNotFoundError: true,         // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  false,        // 禁用彩色打印
+		},
+	)
+}
