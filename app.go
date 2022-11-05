bc16ea30a74e176986a1270c8cfd8302779729e3
liuweike <nervtech@163.com>
2022-11-05 15:56:31 +0800

    Update app.go

--- app.go
+++ app.go
@@ -4,7 +4,6 @@ import (
 	"github.com/kataras/iris/v12"
 	"github.com/siyouyun-open/siyouyun_sdk/entity"
 	"github.com/siyouyun-open/siyouyun_sdk/gateway"
-	siyoumysql "github.com/siyouyun-open/siyouyun_sdk/mysql"
 	"github.com/siyouyun-open/siyouyun_sdk/restclient"
 	"github.com/siyouyun-open/siyouyun_sdk/utils"
 	"gorm.io/driver/mysql"
@@ -41,7 +40,7 @@ func (a *App) init() {
 		panic(err)
 	}
 	db, _ := gorm.Open(mysql.Open(a.AppInfo.DSN), &gorm.Config{
-		Logger: siyoumysql.NewLogger(),
+		//Logger: siyoumysql.NewLogger(),
 	})
 	a.db = db
 }
